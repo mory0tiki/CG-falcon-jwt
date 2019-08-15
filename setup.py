3bebd7649c1e5b0a030ab7a954dc3eb1751ff120
mostafa khaki <mostafa@cheetagroup.com>
2019-08-15 15:12:15 +0430

    UPDATE setup

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open('requirements.txt', 'r') as reqs:
 
 setup(
     name='CheetaJWT',
-    version='1.0',
+    version='1.5',
     packages=find_packages(),
     url='https://github.com/tavanarad/CG-falcon-jwt',
     license='MIS',
-    author='Morteza Tavanarad',
-    author_email='tavanarad@gmail.com',
+    author=['Morteza Tavanarad', 'Mostafa Khaki'],
+    author_email=['tavanarad@gmail.com', 'mostafakhaki00@gmail.com'],
     description='A falcon middleware to handle the JWT header',
     install_requires=install_req
 )
